2391402f6caf1da02b6fbfde1db511ddd7d66b4c
Nabarun21 <ndev@nd.edu>
2018-02-18 11:27:11 -0500

    more models

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,4 +1,4 @@
-python plot_loss_as_metric.py --loss_name MSE --opt_name adadelta --model_name model_v19
-python plot_loss_as_metric.py --loss_name MSE --opt_name adadelta --model_name model_v22
-python plot_loss_as_metric.py --loss_name MSE --opt_name adadelta --model_name model_v23
+python plot_loss_as_metric.py --loss_name MSE --opt_name adadelta --model_name model_v20
+python plot_loss_as_metric.py --loss_name MSE --opt_name adadelta --model_name model_v24
+
 
--- scripts/run2.sh
+++ scripts/run2.sh
@@ -1,4 +1,4 @@
-python plot_losses.py --loss_name MSE --opt_name adadelta --model_name v19
-python plot_losses.py --loss_name MSE --opt_name adadelta --model_name v22
-python plot_losses.py --loss_name MSE --opt_name adadelta --model_name v23
+python plot_losses.py --loss_name MSE --opt_name adadelta --model_name v20
+python plot_losses.py --loss_name MSE --opt_name adadelta --model_name v24
+
 
